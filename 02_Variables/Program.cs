35917d0e9d38ae2691a47f29d06f4e54766f3feb
tigerkaplane <husniyeerparun@gmail.com>
2024-10-02 20:48:40 +0100

    char Variables

--- 02_Variables/Program.cs
+++ 02_Variables/Program.cs
@@ -16,55 +16,66 @@ namespace _02_Variables
             //number = 4.85;
             //Console.WriteLine(number);
 
-            Console.WriteLine("***** Price List *****");
-            Console.WriteLine();
+            //Console.WriteLine("***** Price List *****");
+            //Console.WriteLine();
 
-            double applePrice, orangePrice, strawberryPrice, potatoPrice, tomatoPrice;
+            //double applePrice, orangePrice, strawberryPrice, potatoPrice, tomatoPrice;
 
-            applePrice = 14.85;
-            orangePrice = 20.95;
-            strawberryPrice = 45;
-            potatoPrice = 9.74;
-            tomatoPrice = 6.88;
+            //applePrice = 14.85;
+            //orangePrice = 20.95;
+            //strawberryPrice = 45;
+            //potatoPrice = 9.74;
+            //tomatoPrice = 6.88;
 
-            Console.WriteLine("---- Apple Unit Price:" + " " + applePrice + " " + "TL");
-            Console.WriteLine("---- Orange Unit Price:" + " " + orangePrice + " " + "TL");
-            Console.WriteLine("---- Strawberry Unit Price:" + " " + strawberryPrice + " " + "TL");
-            Console.WriteLine("---- Potato Unit Price:" + " " + potatoPrice + " " + "TL");
-            Console.WriteLine("---- Tomato Unit Price:" + " " + tomatoPrice + " " + "TL");
+            //Console.WriteLine("---- Apple Unit Price:" + " " + applePrice + " " + "TL");
+            //Console.WriteLine("---- Orange Unit Price:" + " " + orangePrice + " " + "TL");
+            //Console.WriteLine("---- Strawberry Unit Price:" + " " + strawberryPrice + " " + "TL");
+            //Console.WriteLine("---- Potato Unit Price:" + " " + potatoPrice + " " + "TL");
+            //Console.WriteLine("---- Tomato Unit Price:" + " " + tomatoPrice + " " + "TL");
 
-            Console.WriteLine();
-            Console.WriteLine();
+            //Console.WriteLine();
+            //Console.WriteLine();
 
-            double appleGram, orangeGram, strawberryGram, potatoGram, tomatoGram;
+            //double appleGram, orangeGram, strawberryGram, potatoGram, tomatoGram;
 
-            appleGram = 1.245;
-            orangeGram = 2.650;
-            strawberryGram = 0.750;
-            potatoGram = 4.859;
-            tomatoGram = 3.745;
+            //appleGram = 1.245;
+            //orangeGram = 2.650;
+            //strawberryGram = 0.750;
+            //potatoGram = 4.859;
+            //tomatoGram = 3.745;
 
-            double appleTotalPrice = appleGram * applePrice;
-            double orangeTotalPrice = orangeGram * orangePrice;
-            double strawberryTotalPrice = strawberryGram * strawberryPrice;
-            double potatoTotalPrice = potatoGram * potatoPrice;
-            double tomatoTotalPrice = tomatoGram * tomatoPrice;
+            //double appleTotalPrice = appleGram * applePrice;
+            //double orangeTotalPrice = orangeGram * orangePrice;
+            //double strawberryTotalPrice = strawberryGram * strawberryPrice;
+            //double potatoTotalPrice = potatoGram * potatoPrice;
+            //double tomatoTotalPrice = tomatoGram * tomatoPrice;
 
-            Console.WriteLine();
-            Console.WriteLine("---------------------------------------------");
-            Console.WriteLine("Item: Apple      - " + "Unit Price:" + " " + applePrice + " - Gr:" + " " + appleGram + " - TotalPrice:" + " " + appleTotalPrice);
-            Console.WriteLine("Item: Orange     - " + "Unit Price:" + " " + orangePrice + " - Gr:" + " " + orangeGram + " - TotalPrice:" + " " + orangeTotalPrice);
-            Console.WriteLine("Item: Strawberry - " + "Unit Price:" + " " + strawberryPrice + " - Gr:" + " " + strawberryGram + " - TotalPrice:" + " " + strawberryTotalPrice);
-            Console.WriteLine("Item: Potato     - " + "Unit Price:" + " " + potatoPrice + " - Gr:" + " " + potatoGram + " - TotalPrice:" + " " + potatoTotalPrice);
-            Console.WriteLine("Item: Tomato     - " + "Unit Price:" + " " + tomatoPrice + " - Gr:" + " " + tomatoGram + " - TotalPrice:" + " " + tomatoTotalPrice);
+            //Console.WriteLine();
+            //Console.WriteLine("---------------------------------------------");
+            //Console.WriteLine("Item: Apple      - " + "Unit Price:" + " " + applePrice + " - Gr:" + " " + appleGram + " - TotalPrice:" + " " + appleTotalPrice);
+            //Console.WriteLine("Item: Orange     - " + "Unit Price:" + " " + orangePrice + " - Gr:" + " " + orangeGram + " - TotalPrice:" + " " + orangeTotalPrice);
+            //Console.WriteLine("Item: Strawberry - " + "Unit Price:" + " " + strawberryPrice + " - Gr:" + " " + strawberryGram + " - TotalPrice:" + " " + strawberryTotalPrice);
+            //Console.WriteLine("Item: Potato     - " + "Unit Price:" + " " + potatoPrice + " - Gr:" + " " + potatoGram + " - TotalPrice:" + " " + potatoTotalPrice);
+            //Console.WriteLine("Item: Tomato     - " + "Unit Price:" + " " + tomatoPrice + " - Gr:" + " " + tomatoGram + " - TotalPrice:" + " " + tomatoTotalPrice);
 
-            double shoppingTotalPrice = appleTotalPrice + orangeTotalPrice + strawberryTotalPrice + potatoTotalPrice + tomatoTotalPrice;
-            Console.WriteLine();
-            Console.WriteLine();
+            //double shoppingTotalPrice = appleTotalPrice + orangeTotalPrice + strawberryTotalPrice + potatoTotalPrice + tomatoTotalPrice;
+            //Console.WriteLine();
+            //Console.WriteLine();
 
-            Console.WriteLine("Total Shopping:" + " " + shoppingTotalPrice + " "+ "TL");
+            //Console.WriteLine("Total Shopping:" + " " + shoppingTotalPrice + " "+ "TL");
             #endregion
 
+            #region Char Variables
+            //Caesar Cipher
+            // no turkish charachter 
+            // " string 'char 
+
+            char symbol;
+            symbol = 'A';
+
+            Console.WriteLine(symbol);
+
+            #endregion
             Console.Read();
 
         }
