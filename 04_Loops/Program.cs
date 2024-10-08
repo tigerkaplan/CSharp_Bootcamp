6f9818aafcb67b0a9aedce939b6298cf69821826
tigerkaplane <husniyeerparun@gmail.com>
2024-10-08 21:40:33 +0100

    For Loop

---
+++ 04_Loops/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Remoting.Services;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _04_Loops
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region For Loop
+            // Donguler 
+            // For (x;y;z)
+            // x: start
+            // y: end
+            // z: increase / decrease 
+
+            //int i;
+            //for (i = 1; i <= 5; i++)
+            //{
+            //    Console.WriteLine("C# Bootcamp");
+            //}
+
+            //for (int i = 1; i <= 20; i++) 
+            //{
+            //    Console.WriteLine(i);
+            //}
+
+            //for (int i = 3; i <= 50 ; i += 3)
+            //{
+            //    Console.WriteLine(i);
+            //}
+
+            //Console.Write("Please, Input  the amount of to print on screen: ");
+            //int finishValue = int.Parse( Console.ReadLine());
+
+            //for (int i = 1; i <= finishValue; i++) 
+            //{
+            //    Console.WriteLine("Happy Birthday");
+            //}
+
+            #endregion
+
+            #region Decision structures with For Loops
+
+            #endregion
+
+            Console.Read();
+        }
+    }
+}
